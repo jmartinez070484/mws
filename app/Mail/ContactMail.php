ede62641207132cbbe76f30d6f5da508df9d6710
Jose Martinez <jmartinez070484@gmail.com>
2020-07-01 16:47:38 -0300

    contact mail

---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $name;
+    public $email;
+    public $subject;
+    public $text;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this -> name = $data['name'];
+        $this -> email = $data['email'];
+        $this -> subject = $data['subject'];
+        $this -> text = $data['message'];
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.contact');
+    }
+}
