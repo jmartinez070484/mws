f7aa80f63dfcb0589e7d3abe0b453f5ddfc61b21
Jose Martinez <jmartinez070484@gmail.com>
2020-09-25 13:37:30 +0300

    immune page

---
+++ resources/views/partials/immune.blade.php
@@ -0,0 +1,16 @@
+<div class="container-md">
+	<div class="row">
+		<section class="trivita-clinic-section col-lg-8 col-md-7">
+			<h1 class="border-under">Get Back to School on Immune Health</h1>
+			<p>This 4-part video series is loaded with important information from Ankit Chander, MD, MD (H), and is a must-have for anyone who wants to maintain a healthy immune system.</p>
+			<div style="margin:20px 0 0 0;padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/442498250?title=0&amp;byline=0&amp;portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+			<div style="margin:20px 0 0 0;padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/442508435?title=0&amp;byline=0&amp;portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+			<div style="margin:20px 0 0 0;padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/442718142?title=0&amp;byline=0&amp;portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+			<div style="margin:20px 0 0 0;padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/442726758?title=0&amp;byline=0&amp;portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe></div>
+		</section>
+		
+		<x-sidebar/>
+	</div>
+</div>
+
+<script src="https://player.vimeo.com/api/player.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/policy','Controller@content') -> name('policy');
 Route::get('/terms','Controller@content') -> name('terms');
 Route::get('/contact','Controller@content') -> name('contact');
 Route::get('/giving-back','Controller@content') -> name('giving');
+Route::get('/immune-health-dvd-6009','Controller@content') -> name('immune');
 
 //mws domain
 Route::get('/{site}','Controller@index') -> name('mws_index');
@@ -34,3 +35,4 @@ Route::get('/{site}/terms','Controller@content') -> name('mws_terms');
 Route::get('/{site}/contact','Controller@content') -> name('mws_contact');
 Route::post('/{site}/contact','Controller@contact');
 Route::get('/{site}/giving-back','Controller@content') -> name('mws_giving');
+Route::get('/{site}/immune-health-dvd-6009','Controller@content') -> name('mws_immune');
